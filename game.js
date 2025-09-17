a4f64bfd40e481b370203f4f7a51d1e84aba0a43
BaconDeGelo <pedro.ugo2015@gmail.com>
2025-09-17 19:59:05 -0300

    Logica (1.0.2)
    Adicionamos umas variaveis fizemos aula de logica pensando nos passos do jogo adicionamos informaçôes html tipo o timer

--- game.js
+++ game.js
@@ -1,2 +1,17 @@
 const canvas = document.getElementById('gamecanvas');
 const ctx = canvas.getContext('2d');
+
+/* VARIAVEIS */
+
+const points = document.getElementById('points');
+const record = document.getElementById('record');
+const time = document.getElementById('timer');
+const errors = document.getElementById('error');
+
+/* VARIAVEIS DE BOTÕES */
+
+const resetgame = document.getElementById('restart');
+
+/* VARIAVEIS DE MENSAGENS */
+
+const youwon = document.getElementById('youwonmsg')
